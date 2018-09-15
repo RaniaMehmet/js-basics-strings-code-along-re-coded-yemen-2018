4eff6c290d86671c4eba0f95feefc13316dd1f2f
Rania Najjar <mehmetrania@gmail.com>
2018-09-15 12:19:01 +0000

    String Cose-along

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+currentUser='Rania';
+welcomeMessage='contains "Welcome to Flatbook, "'+currentUser+'!';
+excitedWelcomeMessage='WELCOME TO FLATBOOK, '+currentUser.toUpperCase()+'!';
+shortGreeting='Welcome, '+currentUser[0]+'!';

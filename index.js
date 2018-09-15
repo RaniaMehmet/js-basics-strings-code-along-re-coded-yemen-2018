c2eca978a40e87bec42d76aeeb0e43296aee352e
Rania Najjar <mehmetrania@gmail.com>
2018-09-15 12:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 currentUser='Rania';
-welcomeMessage='contains "Welcome to Flatbook, "'+currentUser;
+welcomeMessage='contains "Welcome to Flatbook, "'+currentUser+'!';
